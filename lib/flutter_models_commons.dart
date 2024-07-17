1b73c8cb8a3ceb0ebc7d8c16b5209a38b3606f27
Martin jimenez <majiar16@gmail.com>
2024-07-16 20:37:11 -0500

    add show UserModel

--- lib/flutter_models_commons.dart
+++ lib/flutter_models_commons.dart
@@ -3,4 +3,4 @@ library flutter_models_commons;
 export 'models/cart_api_model.dart';
 export 'models/cart_ui_model.dart';
 export './models/product_model.dart';
-export './models/user_model.dart';
+export './models/user_model.dart' show UserModel, Name, Address, Geolocation;
