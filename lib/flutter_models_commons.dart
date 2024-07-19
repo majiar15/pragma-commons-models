de6986e9c712278d631e0a83382334c1825b12c2
Martin jimenez <majiar16@gmail.com>
2024-07-18 20:57:10 -0500

    add discounted model

--- lib/flutter_models_commons.dart
+++ lib/flutter_models_commons.dart
@@ -4,3 +4,4 @@ export 'models/cart_api_model.dart' show CartApiModel, ProductCartApi;
 export 'models/cart_ui_model.dart'show CartUIModel, ProductCartUIModel;
 export './models/product_model.dart' show ProductModel, Rating;
 export './models/user_model.dart' show UserModel, Name, Address, Geolocation;
+export './models/discounted_product_model.dart';
---
+++ lib/models/discounted_product_model.dart
@@ -0,0 +1,62 @@
+
+
+import 'package:flutter_models_commons/flutter_models_commons.dart';
+
+class DiscountedProduct extends ProductModel {
+  final int discountPercentage;
+
+  DiscountedProduct({
+    int? id,
+    required String title,
+    required double price,
+    required String description,
+    required String category,
+    required String image,
+    required Rating rating,
+    required this.discountPercentage,
+  }) : super(
+          id: id,
+          title: title,
+          price: price,
+          description: description,
+          category: category,
+          image: image,
+          rating: rating,
+        );
+
+
+  @override
+  DiscountedProduct copyWith({
+    int? id,
+    String? title,
+    double? price,
+    String? description,
+    String? category,
+    String? image,
+    Rating? rating,
+    int? discountPercentage,
+  }) =>
+      DiscountedProduct(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        price: price ?? this.price,
+        description: description ?? this.description,
+        category: category ?? this.category,
+        image: image ?? this.image,
+        rating: rating ?? this.rating,
+        discountPercentage: discountPercentage ?? this.discountPercentage,
+      );
+
+  factory DiscountedProduct.fromProductModel(ProductModel product, int discountPercentage) {
+    return DiscountedProduct(
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      description: product.description,
+      category: product.category,
+      image: product.image,
+      rating: product.rating,
+      discountPercentage: discountPercentage,
+    );
+  }
+}
